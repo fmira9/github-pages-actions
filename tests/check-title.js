3866e9f3d65a5c2e0b5bcb5ca873967ec13ee601
fmira9 <fmira@deloitte.pt>
2024-11-05 16:55:53 +0000

    Update check-title.js

--- tests/check-title.js
+++ tests/check-title.js
@@ -11,10 +11,10 @@ const titleElement = dom.window.document.querySelector('title');
 if (titleElement && titleElement.textContent.trim()) {
   console.log('✅ Title tag is present and not empty');
   process.exit(0);
+} else if (!titleElement) {
+  console.error('❌ Title tag is missing');
+  process.exit(1); // Exit with error code
 } else if (!titleElement.textContent.trim()) {
   console.error('❌ Title tag is empty');
   process.exit(1); // Exit with error code
-} else {
-  console.error('❌ Title tag is missing');
-  process.exit(1); // Exit with error code
-}
+} 
